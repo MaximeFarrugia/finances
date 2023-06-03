9d7d536fd742aea1e72ce4fe62744e7e969e7e35
Maxime FARRUGIA <maxime.farrugia@smsmode.com>
2023-06-03 15:03:47 +0200

    front: eslint / prettier setup

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+    react: {
+      version: '18.x',
+    },
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        endOfLine: 'lf',
+        singleQuote: true,
+        tabWidth: 2,
+        indentStyle: 'space',
+        useTabs: false,
+        trailingComma: 'all',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+}
